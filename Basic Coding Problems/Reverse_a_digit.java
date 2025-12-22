7cbf49460a3fe5fb10c2ae73463d883ec0af871e
gopichandubolisetty <gopichandubolisetty@gmail.com>
2025-12-22 19:30:13 +0530

    Add the  Reverse a digit

---
+++ Basic Coding Problems/Reverse_a_digit.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Reverse_a_digit {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int rev=0;
+        System.out.println("Number before reversing : "+n);
+        while(n>0){
+            rev = rev*10+n%10;
+            n/=10;
+        }
+        System.out.println("Reversed Number is : "+rev);
+        sc.close();
+    }
+}
