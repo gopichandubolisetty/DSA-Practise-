bf192535ff15575116234823b3433c3dd3d0fc9d
gopichandubolisetty <gopichandubolisetty@gmail.com>
2025-12-22 20:14:28 +0530

    Add the dice problem

---
+++ Basic Coding Problems/Check_the_perfect_array.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Check_the_perfect_array {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[] arr = {1,8,8,8,3,2};
+        int i=0;
+        
+    }
+}
