fda2e4612bab70edcf9bb6ffee8d290f9f2e35a2
gopichandubolisetty <gopichandubolisetty@gmail.com>
2025-12-23 10:01:52 +0530

    Add the Number of divisors

---
+++ Basic Coding Problems/simple_intrest.java
@@ -0,0 +1,9 @@
+public class simple_intrest {
+    public static void main(String[] args){
+        int pri = 1000;
+        int rate = 5;
+        int time =2;
+        double si = (pri*rate*time)/100;
+        System.out.println("The simple intrest is : "+si);
+    }
+}
