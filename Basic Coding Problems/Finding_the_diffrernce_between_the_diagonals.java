6657ecb09313f10a1e9a83a7a97fb32fe0269f90
gopichandubolisetty <gopichandubolisetty@gmail.com>
2025-12-23 15:15:55 +0530

    Add the finding the difference between the primary and secondary diagonals elements in matrix

---
+++ Basic Coding Problems/Finding_the_diffrernce_between_the_diagonals.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Finding_the_diffrernce_between_the_diagonals {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[][] m1 = new int[4][4];
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                m1[i][j] = sc.nextInt();
+            }
+            System.out.println();
+        }
+
+        int primd=0;
+        int secd=0;
+        for(int i=0;i<4;i++){
+            primd+=m1[i][i];
+            secd+=m1[i][3-i];
+        }
+        System.out.println("The difference between the primaray and secondary is : "+(primd-secd));
+    }
+}
