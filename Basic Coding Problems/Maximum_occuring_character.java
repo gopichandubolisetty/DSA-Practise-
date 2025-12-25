961d9834af9829deeff9bed181d3357a61632e6e
gopichandubolisetty <gopichandubolisetty@gmail.com>
2025-12-25 11:14:51 +0530

    Add the Maximum characters occuring in string

---
+++ Basic Coding Problems/Maximum_occuring_character.java
@@ -0,0 +1,19 @@
+public class Maximum_occuring_character {
+    public static void main(String[] args){
+        String in = "geeksforgeeks";
+        int[] count = new int[256];
+        for(int i=0;i<in.length();i++){
+            count[in.charAt(i)]++;
+        }
+
+        int max = -1;
+        char result = ' ';
+         for(int i=0;i<in.length();i++){
+            if(max<count[in.charAt(i)]){
+                max = count[in.charAt(i)];
+                result = in.charAt(i);
+            }
+         }
+         System.out.println("The maximum times repeated letter is : "+result);
+    }
+}
