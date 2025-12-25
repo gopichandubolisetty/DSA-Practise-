903741acf470ee7baef82177198db5dab6175688
gopichandubolisetty <gopichandubolisetty@gmail.com>
2025-12-25 12:24:07 +0530

    Add the replace all 0's with 5

---
+++ Basic Coding Problems/Replace_all_0_with_5_in_an_integer.java
@@ -0,0 +1,23 @@
+public class Replace_all_0_with_5_in_an_integer {
+    public static void main(String[] args){
+        int n = 102;
+        int temp = 0;
+        while(n>0){
+            if(n%10==0){
+                temp = temp*10+5;
+            }else{
+                temp = temp*10+n%10;
+            }
+            n/=10;
+        }
+        System.out.println(temp);
+        int newn=0;
+
+        while(temp > 0){
+        newn = newn *10 + temp%10;
+        temp/=10;
+        }
+        System.out.println(newn);
+    }
+    
+}
