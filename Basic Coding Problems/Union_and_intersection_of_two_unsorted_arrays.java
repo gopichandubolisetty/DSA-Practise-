80f144ea665ce72c2fbb69de7710469fc1cc7f0e
gopichandubolisetty <gopichandubolisetty@gmail.com>
2025-12-26 14:35:41 +0530

    Add the union and intersection of two unsorted arrays

---
+++ Basic Coding Problems/Union_and_intersection_of_two_unsorted_arrays.java
@@ -0,0 +1,44 @@
+public class Union_and_intersection_of_two_unsorted_arrays {
+    public static void main(String[] args){
+        int[] a1 = {1,2,3,4,5,6};
+        int[] a2 = {1,2,3,4,5,6};
+        int len = a1.length+a2.length;
+        int[] re = new int[len];
+        int k=0;
+        for(int i=0;i<a1.length;i++){
+            if(k<len){
+                boolean bo = false;
+                for(int j=0;j<=k;j++){
+                    if(a1[i] == re[j]){
+                        bo = true;
+                        break;
+                    }
+                }
+                if(!bo){
+                    re[k] = a1[i];
+                    k++;
+                }
+            }
+        }
+
+        for(int i=0;i<a2.length;i++){
+            if(k<len){
+                boolean bo = false;
+                for(int j=0;j<=k;j++){
+                    if(a2[i] == re[j]){
+                        bo = true;
+                        break;
+                    }
+                }
+                if(!bo){
+                    re[k] = a2[i];
+                    k++;
+                }
+            }
+        }
+
+        for(int i=0;i<k;i++){
+            System.out.println(re[i]);
+        }
+    }
+}
