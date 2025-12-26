cfdcb13a11731253ec5b7911ff83c2f6412ff495
gopichandubolisetty <gopichandubolisetty@gmail.com>
2025-12-26 11:29:18 +0530

    Add the square root of a number

---
+++ Basic Coding Problems/Square_root_of_a_number.java
@@ -0,0 +1,6 @@
+public class Square_root_of_a_number {
+    public static void main(String[] args){
+        int n =4;
+        System.out.println("The square root of the number is : "+ Math.pow(n,0.5));
+    }
+}
