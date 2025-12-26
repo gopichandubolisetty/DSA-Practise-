7c9baa6954ecd56a2e1f8ef9682417a1c35dc209
gopichandubolisetty <gopichandubolisetty@gmail.com>
2025-12-26 09:59:18 +0530

    Add the alternative merge two strings

---
+++ Basic Coding Problems/Merge_two_Strings.java
@@ -0,0 +1,24 @@
+public class Merge_two_Strings {
+    public static void main(String[] args){
+        String s1 = "hello";
+        String s2 = "geeks";
+        String result = "";
+        int len = (s1.length()>s2.length()) ? s1.length() : s2.length();
+        int i=0;
+        while(i>=0){
+            if(s1.length()>i){
+                result+=s1.charAt(i);
+            }
+            if(s2.length()>i){
+                result+=s2.charAt(i);
+            }
+            
+            if(i<=len-1){
+                i++;
+            }else{
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+}
