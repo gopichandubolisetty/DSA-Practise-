4c6cdc187b8e27b9b1caf9d5de0fbe02e3077708
gopichandubolisetty <gopichandubolisetty@gmail.com>
2025-12-27 16:44:32 +0530

    Add the choose element having max and min in an array

---
+++ Basic Coding Problems/choose_elements_having_max_and_min.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class choose_elements_having_max_and_min {
+    public static void main(String[] args){
+        int[] n = {7,3,2,4,9,12,56};
+        Arrays.sort(n);
+        int m =3;
+        int diff = Integer.MAX_VALUE;
+
+        for(int i=0;i+m-1<n.length;i++){
+            if(n[i+m-1]-n[i]<diff){
+                diff = n[i+2]-n[i];
+            }
+        }
+
+        System.out.println(diff);
+    }
+}
