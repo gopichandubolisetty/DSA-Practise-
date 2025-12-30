e5d4a695e9f384fdde924a9182c1e480da9a9622
gopichandubolisetty <gopichandubolisetty@gmail.com>
2025-12-30 22:15:12 +0530

    Add the Reverse of a Number

---
+++ Learn the basics/Know Basic Math/Reverse_a_Number.java
@@ -0,0 +1,11 @@
+public class Reverse_a_Number {
+    public static void main(String[] args){
+        int n = 35400;
+        int rev = 0;
+        while(n>0){
+            rev = rev*10 + n%10;
+            n=n/10;
+        }
+        System.out.println(rev);
+    }
+}
