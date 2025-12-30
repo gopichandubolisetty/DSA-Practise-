da6f78ebe563c0be8193a370d7a537fa17c1c7b5
gopichandubolisetty <gopichandubolisetty@gmail.com>
2025-12-30 21:57:41 +0530

    Add the short version of the count of the number

---
+++ Learn the basics/Know Basic Math/Count_digits.java
@@ -0,0 +1,10 @@
+public class Count_digits {
+    public static void main(String[] args){
+        int n = 35413655;
+        if(n==0){
+            System.out.println(1);
+        }
+        int count = (int)(Math.log10(n)+1);
+        System.out.println(count);
+    }
+}
