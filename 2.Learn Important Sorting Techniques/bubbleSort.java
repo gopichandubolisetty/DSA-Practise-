f5cfb9230a4302d1242e6bc7a83f60f46a1a7976
gopichandubolisetty <gopichandubolisetty@gmail.com>
2026-01-22 13:16:30 +0530

    Add the bubble sort

--- 2.Learn Important Sorting Techniques/bubbleSort.java
+++ 2.Learn Important Sorting Techniques/bubbleSort.java
@@ -1,26 +1,26 @@
-// public class bubbleSort {
-//     public static void main(String[] args){
-//         int[] arr = {13,46,24,52,20,9};
-//         int n = arr.length;
-//         for(int i=0;i<n-1;i++){
-//             for(int j=0;j<n-1;j++){
-//                 if(arr[j]>arr[j+1]){
-//                     int temp = arr[j];
-//                     arr[j] = arr[j+1];
-//                     arr[j+1] = temp;
-//                 }
-//             }
-//         }
-
-
-//         for(int i=0;i<arr.length-1;i++){
-//             System.out.print(arr[i]+" ");
-//         }
-//     }
-// }
-
-public class bubbleSort{
+public class bubbleSort {
     public static void main(String[] args){
-        int[] arr = {}
+        int[] arr = {13,46,24,52,20,9};
+        int n = arr.length;
+        for(int i=0;i<n-1;i++){
+            boolean check = false;
+            for(int j=0;j<n-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    check=true;
+                }
+
+                if(!check){
+                    break;
+                }
+            }
+        }
+
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
     }
 }
