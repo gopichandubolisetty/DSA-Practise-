aad1c7aa8c04053b62a82da23a2b26906699a399
gopichandubolisetty <gopichandubolisetty@gmail.com>
2026-01-22 12:29:57 +0530

    Modify the selection sort code

--- 2.Learn Important Sorting Techniques/bubbleSort.java
+++ 2.Learn Important Sorting Techniques/bubbleSort.java
@@ -1,20 +1,26 @@
-public class bubbleSort {
+// public class bubbleSort {
+//     public static void main(String[] args){
+//         int[] arr = {13,46,24,52,20,9};
+//         int n = arr.length;
+//         for(int i=0;i<n-1;i++){
+//             for(int j=0;j<n-1;j++){
+//                 if(arr[j]>arr[j+1]){
+//                     int temp = arr[j];
+//                     arr[j] = arr[j+1];
+//                     arr[j+1] = temp;
+//                 }
+//             }
+//         }
+
+
+//         for(int i=0;i<arr.length-1;i++){
+//             System.out.print(arr[i]+" ");
+//         }
+//     }
+// }
+
+public class bubbleSort{
     public static void main(String[] args){
-        int[] arr = {13,46,24,52,20,9};
-        int n = arr.length;
-        for(int i=0;i<n-1;i++){
-            for(int j=0;j<n-1;j++){
-                if(arr[j]>arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
-            }
-        }
-
-
-        for(int i=0;i<arr.length-1;i++){
-            System.out.print(arr[i]+" ");
-        }
+        int[] arr = {}
     }
 }
--- 2.Learn Important Sorting Techniques/selectionSort.java
+++ 2.Learn Important Sorting Techniques/selectionSort.java
@@ -1,32 +1,20 @@
-// class selectionSort{
-//     public static void main(String[] args){
-//         int[] arr = {13,46,24,52,9,20,9};
-//         for(int i=0;i<arr.length-1;i++){
-//             int min=i;
-//             for(int j=i+1;j<arr.length;j++){
-//                 if(arr[min]>arr[j]){
-//                     min=j;
-//                 }
-//             }
-//             int temp = arr[i];
-//             arr[i]=arr[min];
-//             arr[min]=temp;
-//         }
-
-//         for(int i=0;i<arr.length;i++){
-//             System.out.println(arr[i]+" ");
-//         }
-//     }
-// }
-
 class selectionSort{
     public static void main(String[] args){
         int[] arr = {1,5,7,6,3,47,7,2};
         int n = arr.length;
-        for(int i=0;i<n;i++){
+        if(n==0){
+            System.out.println("The size of the array is "+n);
+            return;
+        }
+        if(n==1){
+            System.out.println("The size of the array is "+n);
+            System.out.println(arr[0]);
+            return;
+        }
+        for(int i=0;i<n-1;i++){
             int ind=i;
-            for(int j=1;j<n;j++){
-                if(arr[j]<arr[i]){
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[ind]){
                    ind =j;
                 }
             }
