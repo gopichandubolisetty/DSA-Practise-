37a4023f5eb2c5ce0683d3f930294981f3468539
gopichandubolisetty <gopichandubolisetty@gmail.com>
2026-01-27 13:15:21 +0530

    Modify the LeftRotateAnArrayByDPlaces code

--- Solve Problems on Arrays/Easy/LeftRotateAnArrayByDPlaces.java
+++ Solve Problems on Arrays/Easy/LeftRotateAnArrayByDPlaces.java
@@ -3,21 +3,32 @@ public class LeftRotateAnArrayByDPlaces {
     public static void printArray(int[] n){
         for(int i=0;i<n.length;i++){
             System.out.print(n[i]+" ");
+        } System.out.println();
+    }
+
+    public static void reverseArray(int[] arr,int st,int end){
+        while(st<end){
+            int temp = arr[st];
+            arr[st]=arr[end];
+            arr[end]=temp;
+            st++;
+            end--;
         }
     }
 
     public static void main(String[] args){
         int[] arr ={1,2,3,4,5,6,7};
-        int d = 4;int n = arr.length;
-        int p1=0;int p2=d-1;
-        while(p2<n){
-            int temp = arr[p1];
-            arr[p1] = arr[p2];
-            arr[p2]=temp;
-            p1++;p2++;
-        }
-
+        int d = 4;
+        int n = arr.length;
+        d=d%n;
+        System.out.print("Array before reversing: ");
         printArray(arr);
+        reverseArray(arr,0,d-1);
+        reverseArray(arr,d,n-1);
+        reverseArray(arr,0,n-1);
+        System.out.print("Array after reversing: ");
+        printArray(arr);
+       
     }
 }
 
