2f60bc295792cb70a1314d2049067c14c9d43d4f
gopichandubolisetty <gopichandubolisetty@gmail.com>
2026-01-28 17:44:50 +0530

    Add the MoveAllZerosToEnd code

--- Solve Problems on Arrays/Easy/MoveAllZerosToEnd.java
+++ Solve Problems on Arrays/Easy/MoveAllZerosToEnd.java
@@ -8,16 +8,16 @@ public class MoveAllZerosToEnd {
     public static void main(String[] args){
         int[] arr =  {1 ,0 ,2 ,3 ,0 ,4 ,0 ,1};
         int n = arr.length;
-        int p1=-1;
-        for(int p2=0;p2<n;p2++){
-            if(arr[p2]==0 && p1==-1){
-                p1=p2;
-            }else{
-                int temp = arr[p2];
-                arr[p2]=arr[p1];
-                arr[p1]=temp;
-                p1=p2;
+        int c =0;
+        for(int i=0;i<n;i++){
+            if(arr[i]!=0){
+                arr[c] = arr[i];
+                c++;
             }
+        }
+        while(c<n){
+            arr[c]=0;
+            c++;
         }
         printArray(arr);
     }
@@ -25,17 +25,7 @@ public class MoveAllZerosToEnd {
 
 
                                                                     // Own Methods
-// int c =0;
-        // for(int i=0;i<n;i++){
-        //     if(arr[i]!=0){
-        //         arr[c] = arr[i];
-        //         c++;
-        //     }
-        // }
-        // while(c<n){
-        //     arr[c]=0;
-        //     c++;
-        // }
+
         // for(int i=0;i<n;i++){
         //     for(int j=0;j<n-i-1;j++){
         //         if(arr[j]==0){
