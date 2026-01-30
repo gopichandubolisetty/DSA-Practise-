ac332a9ce675c9d10aa9758443c9ea853fdb1063
gopichandubolisetty <gopichandubolisetty@gmail.com>
2026-01-30 21:49:56 +0530

    Modify the UnionOfTwoSortedArays code

--- Solve Problems on Arrays/Easy/UnionOfTwoSortedArays.java
+++ Solve Problems on Arrays/Easy/UnionOfTwoSortedArays.java
@@ -1,34 +1,56 @@
 public class UnionOfTwoSortedArays {
 
-    public static void printArray(int[] n){
-        for(int i=0;i<n.length;i++){
+    public static void printArray(int[] n,int l){
+        for(int i=0;i<l;i++){
             System.out.print(n[i]+" ");
         }
     }
     public static void main(String[] args){
-        int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int[] arr2 = {2, 3, 4, 4, 5, 11, 12};
+        int[] arr1 = {2, 3, 4, 5, 6, 7,7, 8, 9};
+        int[] arr2 = {2, 3, 4, 4, 5, 11, 12,13};
         int n1 = arr1.length;
         int n2 = arr2.length;
         int[] newarray = new int[n1+n2];
         int l=0;
         int p1=0;
-        for(int i=1 ;i<n1;i++){
-            if(arr1[i-1]!=arr1[i]){
-                newarray[l]=arr1[i-1];
+        int p2=0;
+        while(p1<n1 && p2<n2 ){
+            if(arr1[p1]<arr2[p2]){
+                if(l==0 || (newarray[l-1] != arr1[p1])){
+                newarray[l]=arr1[p1];
                 l++;
             }
-        }
-        if(arr1[n1-1]!=arr1[n1-2]){
-            newarray[l] = arr1[n1-1];
-        }
-        int p2=0;
-        while(p1<l){
-            if(newarray[p1]<arr2[p2]){
+            p1++;
+            }else if(arr1[p1]>arr2[p2]){
+                if(l==0 || (newarray[l-1] != arr2[p2])){
+                newarray[l]=arr2[p2];
+                l++;
+            }
+            p2++;
+            }
+            else{
+                if(l==0 || newarray[l-1] != arr1[p1]){
+                    newarray[l] = arr1[p1];
+                    l++;
+                }
                 p1++;
+                p2++;
             }
         }
-        printArray(newarray);
+        while(p1<n1){
+            if (l == 0 || newarray[l - 1] != arr1[p1]) {
+                newarray[l++] = arr1[p1];
+            }
+            p1++;
+            }
+
+        while(p2<n2){
+            if (l == 0 || newarray[l - 1] != arr2[p2]) {
+                newarray[l++] = arr2[p2];
+            }
+            p2++;
+        }
+        printArray(newarray,l);
     }
 }
 
