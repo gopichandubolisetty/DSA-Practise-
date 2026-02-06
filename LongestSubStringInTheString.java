462a072b20ed3095ec01253dd3ce98e37aee92b5
gopichandubolisetty <gopichandubolisetty@gmail.com>
2026-02-06 22:04:06 +0530

    Add the brute force method of LongestSubArrayWithSumKPostiveAndNegative

---
+++ LongestSubStringInTheString.java
@@ -0,0 +1,7 @@
+public class LongestSubStringInTheString {
+    public static void main(String[] args){
+        String word = "abcbabcba";
+        int p1=0;
+        boolean[] check = new boolean[128]
+    }   
+}
---
+++ Solve Problems on Arrays/Easy/LongestSubArrayWithSumKPostiveAndNegative.java
@@ -0,0 +1,30 @@
+public class LongestSubArrayWithSumKPostiveAndNegative {
+    public void printArray(int[] n,int l){
+        for(int i=0;i<l;i++){
+            System.out.print(n[i]+" ");
+        }
+    }
+    public static void main(String[] args){
+        int[] arr = {1,2,3};
+        int sum = 100;
+        int len = arr.length;
+        if(len==0){return;}else if(len==1 && sum==arr[0]){
+            System.out.println("The sum is at : "+0);
+        }
+        int ind1=-1;
+        int ind2=-1;
+        for(int i=0;i<len;i++){
+            int comp=arr[i];
+            for(int j=i+1;j<len;j++){
+                comp+=arr[j];
+                if(comp==sum && (ind2-ind1)<(j-i)){
+                    ind1=i;ind2=j;
+                }
+            }
+            if(ind1==-1 && ind2==-1 && arr[i]==sum){
+                ind1=i;ind2=i;
+            }
+        }
+        System.out.println("The indexes start from ind1: "+ind1+" ind2:"+ind2);
+    }
+}
