23b5bf5202c33db7d6d50734c00207cf5ecd044f
gopichandubolisetty <gopichandubolisetty@gmail.com>
2026-02-20 22:52:30 +0530

    Add the code SpiralMatrix

--- Solve Problems on Arrays/Medium/SpiralMatrix.java
+++ Solve Problems on Arrays/Medium/SpiralMatrix.java
@@ -1,18 +1,45 @@
 public class SpiralMatrix{
     public static void main(String[] args){
         int[][] mat = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
-        int 
+        int top =0;
+        int bottom = mat.length-1;
+        int left = 0;
+        int right = mat[0].length-1;
+
+         while(top<=bottom && left<=right){
+
+            for(int i=left;i<=right;i++){
+                System.out.print(mat[top][i]+" ");
+            }
+            top++;
+
+            for(int i=top;i<=bottom;i++){
+                System.out.print(mat[i][right]+" ");
+            }
+            right--;
+
+            if(top<=bottom){
+                for(int i= right;i>=left;i--){
+                    System.out.print(mat[bottom][i]+" ");
+                }
+                bottom--;
+            }
+
+            if(left<right){
+                for(int i=bottom;i>=top;i--){
+                    System.out.print(mat[i][left]+" ");
+                }
+                left++;
+            }
+         }
     }
-} 
+}
 
 
 
 
-
-
-
-                                                    // Brute Force Approach
-        // int p1=0;int p2=rowlen;
+            // Brute Force
+// int p1=0;int p2=rowlen;
         //  while(p1<=p2){
         //     for(int i=p1;i<=collen-p1;i++){
         //         System.out.print(mat[p1][i]+" ");
