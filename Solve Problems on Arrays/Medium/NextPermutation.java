c3aa011e4f1546afc398c84a005544f37dd61397
gopichandubolisetty <gopichandubolisetty@gmail.com>
2026-02-20 21:10:15 +0530

    Add the code NextPermutation

--- Solve Problems on Arrays/Medium/NextPermutation.java
+++ Solve Problems on Arrays/Medium/NextPermutation.java
@@ -1,21 +1,37 @@
 public class NextPermutation {
 
-    public static int CalculateFact(int n){
-        if(n<=0){
-            return 1;
-        }
-        return n*CalculateFact(n-1);
-    }
     public static void printArray(int[] arr){
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
     }
+
+    public static void reverseArray(int[] arr,int st,int en){
+        int p1=st;int p2=en;
+        while(p1<p2){
+            int temp = arr[p1];
+            arr[p1]=arr[p2];
+            arr[p2]=temp;
+            p1++;p2--;
+        }
+    }
     public static void main(String[] args){
         int[] arr = {1,3,2};
-        int n = arr.length;
-        int fact = CalculateFact(n);
-        int[] prem = new int[fact];
-        
+        int n = arr.length-2;
+        int len = arr.length-1;
+        while(n>=0 && arr[n]>=arr[n+1]){
+            n--;
+        }
+        if(n>=0){
+            while(arr[len]<=arr[n]){
+                len--;
+            }
+            System.out.println("Len: "+len+" "+"n: "+n);
+            int temp = arr[n];
+            arr[n]=arr[len];
+            arr[len]=temp;
+        }
+        reverseArray(arr,n+1,arr.length-1);
+        printArray(arr);
     }
 }
--- Solve Problems on Arrays/Medium/SpiralMatrix.java
+++ Solve Problems on Arrays/Medium/SpiralMatrix.java
@@ -1,24 +1,30 @@
 public class SpiralMatrix{
     public static void main(String[] args){
-        int[][] mat = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-        int rowlen = mat.length-1;
-        int collen = mat[0].length-1;
-
-        int p1=0;int p2=rowlen;
-         while(p1<p2){
-            for(int i=p1;i<=collen-p1;i++){
-                System.out.print(mat[p1][i]+" ");
-            }
-            for(int i=p1+1;i<=rowlen-p1;i++){
-                System.out.print(mat[i][collen-p1]+" ");
-            }
-            for(int i=collen-1-p1;i>=p1;i--){
-                System.out.print(mat[p2][i]+" ");
-            }
-            for(int i=rowlen-1-p1;i>p1;i--){
-                System.out.print(mat[i][p1]+" ");
-            }
-            p1++;p2--;
-         }
+        int[][] mat = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        int 
     }
-}
+} 
+
+
+
+
+
+
+
+                                                    // Brute Force Approach
+        // int p1=0;int p2=rowlen;
+        //  while(p1<=p2){
+        //     for(int i=p1;i<=collen-p1;i++){
+        //         System.out.print(mat[p1][i]+" ");
+        //     }
+        //     for(int i=p1+1;i<=rowlen-p1;i++){
+        //         System.out.print(mat[i][collen-p1]+" ");
+        //     }
+        //     for(int i=collen-1-p1;i>=p1;i--){
+        //         System.out.print(mat[p2][i]+" ");
+        //     }
+        //     for(int i=rowlen-1-p1;i>p1;i--){
+        //         System.out.print(mat[i][p1]+" ");
+        //     }
+        //     p1++;p2--;
+        //  }
