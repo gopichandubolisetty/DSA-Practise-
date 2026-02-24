306c4a70e893d6c4b9dcb6ba4c5e08bb182cfcf7
gopichandubolisetty <gopichandubolisetty@gmail.com>
2026-02-24 10:30:10 +0530

    Add the optimal code LongestSubArrayWithSumKPostiveAndNegative

--- Solve Problems on Arrays/Easy/LongestSubArrayWithSumKPostiveAndNegative.java
+++ Solve Problems on Arrays/Easy/LongestSubArrayWithSumKPostiveAndNegative.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class LongestSubArrayWithSumKPostiveAndNegative {
     public void printArray(int[] n,int l){
         for(int i=0;i<l;i++){
@@ -6,25 +8,49 @@ public class LongestSubArrayWithSumKPostiveAndNegative {
     }
     public static void main(String[] args){
         int[] arr = {1,-2,-3,0,3,2,2,3};
-        int sum = 3;
-        int len = arr.length;
-        if(len==0){return;}else if(len==1 && sum==arr[0]){
-            System.out.println("The sum is at : "+0);
-        }
-        int ind1=-1;
-        int ind2=-1;
-        for(int i=0;i<len;i++){
-            int comp=arr[i];
-            for(int j=i+1;j<len;j++){
-                comp+=arr[j];
-                if(comp==sum && (ind2-ind1)<(j-i)){
-                    ind1=i;ind2=j;
-                }
+        int sum = 0;
+        int curr=0;
+        int max =0;
+        HashMap<Integer,Integer> m = new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            curr=curr+arr[i];
+            if(curr==sum){
+                max=i+1;
             }
-            if(ind1==-1 && ind2==-1 && arr[i]==sum){
-                ind1=i;ind2=i;
+            int rem = curr-sum;
+            if(m.containsKey(rem)){
+                int len = i-m.get(rem);
+                max = Math.max(max,len);
+            }
+            if(!m.containsKey(curr)){
+                m.put(curr,i);
             }
         }
-        System.out.println("The indexes start from ind1: "+ind1+" ind2:"+ind2);
+
+        System.out.println(max+" "+m);
     }
 }
+
+
+
+
+                                                    // Brute force approach
+// int len = arr.length;
+//         if(len==0){return;}else if(len==1 && sum==arr[0]){
+//             System.out.println("The sum is at : "+0);
+//         }
+//         int ind1=-1;
+//         int ind2=-1;
+//         for(int i=0;i<len;i++){
+//             int comp=arr[i];
+//             for(int j=i+1;j<len;j++){
+//                 comp+=arr[j];
+//                 if(comp==sum && (ind2-ind1)<(j-i)){
+//                     ind1=i;ind2=j;
+//                 }
+//             }
+//             if(ind1==-1 && ind2==-1 && arr[i]==sum){
+//                 ind1=i;ind2=i;
+//             }
+//         }
+//         System.out.println("The indexes start from ind1: "+ind1+" ind2:"+ind2);
--- Solve Problems on Arrays/Hard/LongestSubArrayWithZero.java
+++ Solve Problems on Arrays/Hard/LongestSubArrayWithZero.java
@@ -1,21 +1,30 @@
+import java.util.*;
 public class LongestSubArrayWithZero {
     public static void main(String[] args){
         int[] nums = {9,-3,3,-1,6,-5};
         int length = nums.length;
         int ind1=-1,ind2=-1;
-        for(int i=0;i<length-1;i++){
-            int sum=nums[i];
-            if(sum==0){
-                System.out.println("The sub array with ind1: "+ind1+" ind2: "+ind2);
-            }
-            for(int j=i+1;j<length;j++){
-                sum=sum+nums[j];
-                if(sum==0){
-                    System.out.println("The indexes are at "+i+" "+(j));
-                    if(j-i > ind2-ind1){ind1=i;ind2=j;}
-                }
-            }
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<length;i++){
+            map.put(i,nums[i]);
         }
-        System.out.println("The longest array indexes are at ind1: "+ind1+" ind2: "+ind2);
+        System.out.println(map);
     }
 }
+
+
+                                                                    // Brute Force Approach
+// for(int i=0;i<length-1;i++){
+        //     int sum=nums[i];
+        //     if(sum==0){
+        //         System.out.println("The sub array with ind1: "+ind1+" ind2: "+ind2);
+        //     }
+        //     for(int j=i+1;j<length;j++){
+        //         sum=sum+nums[j];
+        //         if(sum==0){
+        //             System.out.println("The indexes are at "+i+" "+(j));
+        //             if(j-i > ind2-ind1){ind1=i;ind2=j;}
+        //         }
+        //     }
+        // }
+        // System.out.println("The longest array indexes are at ind1: "+ind1+" ind2: "+ind2);
