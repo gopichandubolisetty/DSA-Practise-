a9f10989fd523f69796e738197ff32f4d4898573
gopichandubolisetty <gopichandubolisetty@gmail.com>
2026-03-04 21:34:35 +0530

    Add the bruteforce code for the MergeOverlappingIntervals

---
+++ Solve Problems on Arrays/Hard/MergeOverlappingIntervals.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class MergeOverlappingIntervals {
+    public static void printArray(int[][] arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+            System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        int[][] nums = {{1,3},{2,6},{8,10},{15,18}};
+        int rowlen = nums.length;
+        int collen = nums[0].length;
+        int[][] order = new int[rowlen][collen];
+        // System.out.println(rowlen+" "+collen);
+        printArray(nums);
+        Arrays.sort(nums, (a, b) -> Integer.compare(a[0], b[0]));
+        printArray(nums);
+        for(int i=0;i<rowlen;i++){
+            for(int j=0;j<collen;j++){
+                if()
+            }
+        }
+    }
+}
