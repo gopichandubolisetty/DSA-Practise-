7f5c40a825dafe94e4df86a4bfb46014f380230a
gopichandubolisetty <gopichandubolisetty@gmail.com>
2026-04-09 22:45:56 +0530

    Modify the code with StringBuilder of Remove Outermost Parentheses

--- 5.Strings[Basic Medium]/Basic and Easy String Problems/RemoveOutermostParentheses.java
+++ 5.Strings[Basic Medium]/Basic and Easy String Problems/RemoveOutermostParentheses.java
@@ -1,21 +1,23 @@
 public class RemoveOutermostParentheses {
     public static void main(String[] args){
         String paren = "(()())(())(()(()))";
+        StringBuilder result = new StringBuilder();
         int count = 0;
         for(int i=0;i<paren.length();i++){
             
             if(paren.charAt(i)=='('){
                 if(count>0){
-                    System.out.print(paren.charAt(i));
+                    result.append(paren.charAt(i));
                 }
                 count++;
             }else{
                 count--;
                 if(count>0){
-                    System.out.print(paren.charAt(i));
+                    result.append(paren.charAt(i));
                 }
             }
         }
+        System.out.println(result);
     }
 }
 
