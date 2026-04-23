9e9d7ab72cc777d7a72e06e0c4034734b3b756d1
gopichandubolisetty <gopichandubolisetty@gmail.com>
2026-04-23 23:31:51 +0530

    Modify the code for the Insertion At The Head Of Linked List

--- 6.Learn LinkedList[Single LL, Double LL,Medium, Hard Problems]/Learn 1D LinkedList/InsertionAtTheHeadOfLinkedList.java
+++ 6.Learn LinkedList[Single LL, Double LL,Medium, Hard Problems]/Learn 1D LinkedList/InsertionAtTheHeadOfLinkedList.java
@@ -1,27 +1,21 @@
-class Node {
+public class InsertionAtTheHeadOfLinkedList {
     int data;
-    Node next;
+    InsertionAtTheHeadOfLinkedList next;
 
-    Node(int data1, Node next1) {
+
+    InsertionAtTheHeadOfLinkedList(int data1, InsertionAtTheHeadOfLinkedList next1) {
         this.data = data1;
         this.next = next1;
     }
 
-    Node(int data1) {
+    InsertionAtTheHeadOfLinkedList(int data1) {
         this.data = data1;
         this.next = null;
     }
-}
 
-public class InsertionAtTheHeadOfLinkedList {
-
-    public static Node insertAtHead(Node head, int newData) {
-        Node newNode = new Node(newData, head);
-        return newNode;
-    }
-
-    public static void printList(Node head) {
-        Node temp = head;
+    // Static methods for operations
+    public static void display(InsertionAtTheHeadOfLinkedList head) {
+        InsertionAtTheHeadOfLinkedList temp = head;
         while (temp != null) {
             System.out.print(temp.data + " -> ");
             temp = temp.next;
@@ -29,12 +23,21 @@ public class InsertionAtTheHeadOfLinkedList {
         System.out.println("null");
     }
 
+    public static InsertionAtTheHeadOfLinkedList insertHead(int val, InsertionAtTheHeadOfLinkedList head) {
+        return new InsertionAtTheHeadOfLinkedList(val, head);
+    }
+
+   
     public static void main(String[] args) {
-        Node head = new Node(2);
-        head.next = new Node(3);
-
-        head = insertAtHead(head, 1);
-
-        printList(head);
+        InsertionAtTheHeadOfLinkedList head = new InsertionAtTheHeadOfLinkedList(10);
+        head.next = new InsertionAtTheHeadOfLinkedList(20);
+        
+        System.out.print("Before: ");
+        display(head);
+        
+        head = insertHead(5, head);
+        
+        System.out.print("After:  ");
+        display(head);
     }
 }
