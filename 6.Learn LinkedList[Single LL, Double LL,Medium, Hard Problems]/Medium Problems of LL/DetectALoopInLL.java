79443aacbba8855d887f574547e8ff1740bf9efd
gopichandubolisetty <gopichandubolisetty@gmail.com>
2026-04-27 12:13:16 +0530

    Add the code for the Detect a looop in LL

---
+++ 6.Learn LinkedList[Single LL, Double LL,Medium, Hard Problems]/Medium Problems of LL/DetectALoopInLL.java
@@ -0,0 +1,62 @@
+class Node{
+    int data;
+    Node next;
+
+    Node(int data1){
+        this.data=data1;
+        this.next=null;
+    }
+}
+
+
+
+public class DetectALoopInLL {
+
+    public static void printLL(Node head){
+         Node curr = head;
+         while(curr!=null){
+            System.out.print(curr.data);
+            System.out.print("->");
+            curr = curr.next;
+         }
+         System.out.println("null");
+    }
+
+    public static boolean checkloop(Node head){
+
+        Node fast =head;
+        Node slow = head;
+
+        while(fast!=null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast==slow){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args){
+
+        Node head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+        Node fourth = new Node(4);
+        Node fifth = new Node(5);
+
+        head.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+        fifth.next = third;
+
+        boolean ch = checkloop(head);
+        if(ch){
+            System.out.println("It has loop");
+        }else{
+            System.out.print("It has no loop");
+        }
+    }
+
+}
