717d5cb9c0b832d67deaaccf6286e510d755af7c
gopichandubolisetty <gopichandubolisetty@gmail.com>
2026-08-02 00:04:57 +0530

    Add the code for the Check if the Ith bit is set or not

--- 8.Bit Manipulation[Concepts and Problems]/Learn Bit Manipulation/CheckIfTheIthBitIsSetOrNot.java
+++ 8.Bit Manipulation[Concepts and Problems]/Learn Bit Manipulation/CheckIfTheIthBitIsSetOrNot.java
@@ -1,3 +1,14 @@
 public class CheckIfTheIthBitIsSetOrNot {
-    
+    public static void main(String[] args) {
+        int n = 5;
+        int i = 2;
+
+        boolean isSet = (n & (1 << i)) != 0;
+
+        if (isSet) {
+            System.out.println("The " + i + "-th bit of " + n + " is set (1).");
+        } else {
+            System.out.println("The " + i + "-th bit of " + n + " is not set (0).");
+        }
+    }
 }
