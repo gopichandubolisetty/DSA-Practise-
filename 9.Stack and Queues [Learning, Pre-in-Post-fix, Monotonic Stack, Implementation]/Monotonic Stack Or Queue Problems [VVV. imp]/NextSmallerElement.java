1666672529bdb9b31340db6f45d62bdc4491925d
gopichandubolisetty <gopichandubolisetty@gmail.com>
2026-09-01 08:29:36 +0530

    Add the code for the Next Small Element

---
+++ 9.Stack and Queues [Learning, Pre-in-Post-fix, Monotonic Stack, Implementation]/Monotonic Stack Or Queue Problems [VVV. imp]/NextSmallerElement.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+
+
+class Solution {
+    public static int[] nextSmall(int[] nums) {
+        Stack<Integer> st = new Stack<>();
+
+        int n = nums.length;
+        int[] res = new int[n];
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!st.isEmpty() && st.peek() >= nums[i]) {
+                st.pop();
+            }
+
+            if (st.isEmpty()) res[i] = -1;
+            else res[i] = st.peek();
+
+            st.push(nums[i]);
+        }
+
+        return res;
+    }
+}
+
+public class NextSmallerElement {
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 2, 10};
+        int[] ans = Solution.nextSmall(nums);
+
+        for (int x : ans) {
+            System.out.print(x + " ");
+        }
+
+        System.out.println();
+    }
+}
