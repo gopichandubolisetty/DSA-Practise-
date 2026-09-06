aed8c9223104022edb3fe1ae284ada964fc56277
gopichandubolisetty <gopichandubolisetty@gmail.com>
2026-09-06 18:46:19 +0530

    Add the code for the Fruit into basket

---
+++ 10.Sliding Window And Two Pointer Combined Problems/Medium Problems/FruitIntoBasket.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class FruitIntoBasket {
+    public int totalFruit(int[] fruits) {
+        int maxlen = 0;
+        int lastFruit = -1, secondLastFruit = -1;
+        int currCount = 0, lastFruitStreak = 0;
+
+        for (int fruit : fruits) {
+            if (fruit == lastFruit || fruit == secondLastFruit) {
+                currCount++;
+            } else {
+                currCount = lastFruitStreak + 1;
+            }
+
+            if (fruit == lastFruit) {
+                lastFruitStreak++;
+            } else {
+                lastFruitStreak = 1;
+                secondLastFruit = lastFruit;
+                lastFruit = fruit;
+            }
+
+            maxlen = Math.max(maxlen, currCount);
+        }
+
+        return maxlen;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        FruitIntoBasket sol = new FruitIntoBasket();
+        int[] fruits = {1, 2, 1, 2, 3};
+        System.out.println(sol.totalFruit(fruits));
+    }
+}
